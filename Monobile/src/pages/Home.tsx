f91500bc219d337355f8efbbd1287b971bcc98ab
thomasmlc <43410009+thomasmlc@users.noreply.github.com>
2020-11-08 15:59:12 +0100

    Update Home.tsx

--- Monobile/src/pages/Home.tsx
+++ Monobile/src/pages/Home.tsx
@@ -1,25 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonBackButton, IonApp, IonIcon, IonButton } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader >
+    <IonApp>
+      <IonHeader>
         <IonToolbar color="primary">
           <IonTitle>Monobile</IonTitle>
+          <IonButtons slot="end">
+            <IonButton>
+              <IonIcon name="person-outline"></IonIcon>
+            </IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+
+
       </IonContent>
-    </IonPage>
+    </IonApp>
   );
 };
 
