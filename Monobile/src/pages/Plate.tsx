413560d7b971d4b779d00b565f8f1714e5acd3d8
PierreBouffartigue <43409971+PierreBouffartigue@users.noreply.github.com>
2020-11-23 11:31:10 +0100

    Update Plate.tsx

--- Monobile/src/pages/Plate.tsx
+++ Monobile/src/pages/Plate.tsx
@@ -1,49 +1,79 @@
 import React from 'react';
-import {IonButton, IonCol, IonContent, IonGrid, IonRow} from '@ionic/react';
+import {IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar} from '@ionic/react';
 
 export const Plate: React.FC = () => (
-    <IonContent>
-        <IonGrid>
-            <IonRow>
-                <IonCol><IonButton color="primary">Primary</IonButton></IonCol>
-                <IonCol><IonButton color="primary">Primary</IonButton></IonCol>
-                <IonCol><IonButton color="primary">Primary</IonButton></IonCol>
-                <IonCol><IonButton color="primary">Primary</IonButton></IonCol>
-                <IonCol><IonButton color="primary">Primary</IonButton></IonCol>
-                <IonCol><IonButton color="primary">Primary</IonButton></IonCol>
-                <IonCol><IonButton color="primary">Primary</IonButton></IonCol>
-            </IonRow>
-
-            <IonRow>
-                <IonCol size="12" size-md>
-                    <IonButton color="primary">Primary</IonButton>
-                </IonCol>
-                <IonCol size="12" size-md>
-                    <IonButton color="primary">Primary</IonButton>
-                </IonCol>
-                <IonCol size="12" size-md>
-                    <IonButton color="primary">Primary</IonButton>
-                </IonCol>
-                <IonCol size="12" size-md>
-                    <IonButton color="primary">Primary</IonButton>
-                </IonCol>
-                <IonCol>
-                    <IonButton color="primary">Primary</IonButton>
-                </IonCol>
-                <IonCol size="12" size-md>
-                    <IonButton color="primary">Primary</IonButton>
-                </IonCol>
-            </IonRow>
-
-            <IonRow>
-                <IonCol><IonButton color="primary">Primary</IonButton></IonCol>
-                <IonCol><IonButton color="primary">Primary</IonButton></IonCol>
-                <IonCol><IonButton color="primary">Primary</IonButton></IonCol>
-                <IonCol><IonButton color="primary">Primary</IonButton></IonCol>
-                <IonCol><IonButton color="primary">Primary</IonButton></IonCol>
-                <IonCol><IonButton color="primary">Primary</IonButton></IonCol>
-                <IonCol><IonButton color="primary">Primary</IonButton></IonCol>
-            </IonRow>
-        </IonGrid>
-    </IonContent>
+    <IonPage>
+        <IonHeader>
+            <IonToolbar border-color="new" mode="ios">
+                <IonTitle color="new">Plate</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+        <IonContent className="padding" color="new">
+            <IonGrid>
+                <IonRow className="ion-align-items-start">
+                    <IonCol>case</IonCol>
+                    <IonCol>case</IonCol>
+                    <IonCol>case</IonCol>
+                    <IonCol>case</IonCol>
+                    <IonCol>case</IonCol>
+                    <IonCol>case</IonCol>
+                    <IonCol>case</IonCol>
+                </IonRow>
+                <IonRow className="ion-align-items-center">
+                    <IonCol>case</IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol>case</IonCol>
+                </IonRow>
+                <IonRow className="ion-align-items-center">
+                    <IonCol>case</IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol>case</IonCol>
+                </IonRow>
+                <IonRow className="ion-align-items-center">
+                    <IonCol>case</IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol>case</IonCol>
+                </IonRow>
+                <IonRow className="ion-align-items-center">
+                    <IonCol>case</IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol>case</IonCol>
+                </IonRow>
+                <IonRow className="ion-align-items-center">
+                    <IonCol>case</IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol> </IonCol>
+                    <IonCol>case</IonCol>
+                </IonRow>
+                <IonRow className="ion-align-items-end">
+                    <IonCol>case</IonCol>
+                    <IonCol>case</IonCol>
+                    <IonCol>case</IonCol>
+                    <IonCol>case</IonCol>
+                    <IonCol>case</IonCol>
+                    <IonCol>case</IonCol>
+                    <IonCol>case</IonCol>
+                </IonRow>
+            </IonGrid>
+        </IonContent>
+    </IonPage>
 );
