01204aac1f8d1f3f3b1c6f1c92e09679a3da7ac0
PierreBouffartigue <43409971+PierreBouffartigue@users.noreply.github.com>
2020-12-06 18:52:35 +0100

    Add footer on HOME page

--- Monobile/src/pages/Home.tsx
+++ Monobile/src/pages/Home.tsx
@@ -1,4 +1,14 @@
-import {IonApp, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonTitle, IonToolbar, IonBackButton} from '@ionic/react';
+import {
+    IonApp,
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonFooter,
+    IonHeader,
+    IonIcon,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
 import React from 'react';
 import {personOutline} from 'ionicons/icons';
 import {ROUTE_PLATE, ROUTE_PROFILE} from '../nav/Routes';
@@ -11,17 +21,21 @@ const Home: React.FC = () => {
                     <IonTitle>Monobile</IonTitle>
                     <IonButtons slot="end">
                         <IonButton href={ROUTE_PROFILE}>
-                            <IonIcon icon={personOutline}></IonIcon>
+                            <IonIcon icon={personOutline}/>
                         </IonButton>
                     </IonButtons>
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen>
-
-
-                <IonButton size="large" expand="block" color="secondary" href={ROUTE_PLATE}>
-                    JOUER
-                </IonButton>
+                <p><b>Monobile </b> est un Monopoly like, vous devrez vous déplacer sur le plateau et bâtir un empire
+                    financier pour ruiner vos adversaires !</p>
+                <IonFooter>
+                    <IonToolbar>
+                        <IonButton size="large" expand="block" color="secondary" href={ROUTE_PLATE}>
+                            JOUER
+                        </IonButton>
+                    </IonToolbar>
+                </IonFooter>
 
 
             </IonContent>
