a62041ed798c76a329bff414c61bcdb7f5a549b3
calvin <calvin.seaphanh@ynov.com>
2020-12-06 18:06:35 +0100

    change route gameprofiles

--- Monobile/src/nav/Routes.tsx
+++ Monobile/src/nav/Routes.tsx
@@ -1,5 +1,5 @@
 export const ROUTE_TABS_BASE = '/tabs/';
 export const ROUTE_HOME = ROUTE_TABS_BASE + 'home/';
 export const ROUTE_PLATE = ROUTE_TABS_BASE + 'plate/';
-export const ROUTE_PROFILE = ROUTE_TABS_BASE + 'profiles/';
+export const ROUTE_PROFILE = ROUTE_TABS_BASE + 'gameprofiles/';
 export const ROUTE_GAMEPROFILE = ROUTE_TABS_BASE + 'gameprofiles/';
